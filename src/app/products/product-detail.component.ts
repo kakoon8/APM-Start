d3f7e690ea7b6def5bdc067a1ad162db4452cb1e
Andrew <andrewhinkson88@gmail.com>
2020-01-22 00:54:08 -0500

    added guard to prevent access if id is NaN or less than 1

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,17 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
-
+import {ActivatedRoute } from '@angular/router';
+import {Router} from '@angular/router';
 @Component({
   
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  pageTitle:string= 'Product-detail';
+  pageTitle:string= 'Product-Detail';
   product: IProduct;
-  constructor() { }
+  constructor (private route: ActivatedRoute,
+               private router: Router) {}
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      "productId": id,
+    "productName": "Garden Cart",
+    "productCode": "GDN-0023",
+    "releaseDate": "March 18, 2019",
+    "description": "15 gallon capacity rolling garden cart",
+    "price": 32.99,
+    "starRating": 4.2,
+    "imageUrl": "assets/images/garden_cart.png"
+    }
+  }
+  onBack():void{
+    this.router.navigate (['/products']);
   }
 
 }
